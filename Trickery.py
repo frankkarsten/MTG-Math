46d33e477038583169a69ef51d82b8bdd49b8d19
frankkarsten <46165285+frankkarsten@users.noreply.github.com>
2021-02-10 09:58:24 +0100

    Update Trickery.py

--- Trickery.py
+++ Trickery.py
@@ -1,17 +1,13 @@
 import random
 from collections import Counter
 from itertools import combinations
+from datetime import datetime
 
-decklist = {
-	'Trickery': 1,
-	'Outburst': 4,
-	'Emrakul': 4,
-	'Land': 51
-}
 
-### of T3 Emrakul with 2 Emrakul, 53 land: 90.75%
-### of T3 Emrakul with 3 Emrakul, 52 land: 91.46%
-### of T3 Emrakul with 4 Emrakul, 51 land: 91.34%
+### of T3 Emrakul with 2 Emrakul, 53 land: Updated with new, faster sims: 90.819%.
+### of T3 Emrakul with 3 Emrakul, 52 land: Updated with new, faster sims: 91.509%.
+### of T3 Emrakul with 4 Emrakul, 51 land: Updated with new, faster sims: 91.512%.
+### of T3 Emrakul with 5 Emrakul, 51 land: Updated with new, faster sims: 91.400%.
 
 def simulate_one_game(hand, library, drawfirst):
 	"""	
@@ -24,7 +20,7 @@ def simulate_one_game(hand, library, drawfirst):
 	"""
 	
 	#TURN 1 GAMEPLAY SEQUENCE
-
+	
 	#Draw a card if on the draw
 	if (drawfirst):
 		card_drawn = library.pop(0)
@@ -62,10 +58,15 @@ def simulate_one_game(hand, library, drawfirst):
 	#Return True if we were able to combo off, and False otherwise
 	if (land_in_play == 3 and hand['Outburst'] > 0):
 		#Is there still Trickery in deck?
-		if (hand['Trickery'] == 1):
+		if (hand['Trickery'] == decklist['Trickery']):
 			return False
 		else:
-			#Drawing or milling has the same effect for our purposes
+			#Find Trickery in library list
+			above_Trickery = library[:library.index('Trickery')].copy()
+			random.shuffle(above_Trickery)
+			below_Trickery = library[library.index('Trickery') +1:].copy()
+			library = below_Trickery + above_Trickery
+			#Mill 3; drawing or milling has the same effect for our purposes
 			mill_nr = random.randint(1,3)
 			for _ in range(mill_nr):
 				card_drawn = library.pop(0)
@@ -113,6 +114,36 @@ def simulate_one_specific_hand(hand, bottom, drawfirst, num_iterations):
 		
 	return count_good_hands/num_iterations
 
+def binom(n, k):
+	"""	
+	Parameters:
+		n - Number of elements of the entire set
+		k - Number of elements in the subset
+	It should hold that 0 <= k <= n
+	Returns - The binomial coefficient n choose k that represents the number of ways of picking k unordered outcomes from n possibilities
+	"""
+	answer = 1
+	for i in range(1, min(k, n - k) + 1):
+		answer = answer * (n + 1 - i) / i
+	return int(answer)
+
+def multivariate_hypgeom(deck, needed):
+	"""	
+	Parameters:
+		deck - A dictionary of cardname : number of copies
+		needed - A dictionary of cardname : number of copies
+	It should hold that the cardname keys of deck and needed are identical
+	Returns - the multivariate hypergeometric probability of drawing exactly the cards in 'needed' from 'deck' when drawing without replacement 
+	"""
+	answer = 1
+	sum_deck = 0
+	sum_needed = 0
+	for card in deck.keys():
+		answer *= binom(deck[card], needed[card])
+		sum_deck += deck[card]
+		sum_needed += needed[card]
+	return answer / binom(sum_deck, sum_needed)
+	
 
 def what_to_put_on_bottom (hand, drawfirst, number_bottom, num_iterations):
 	"""	
@@ -133,8 +164,8 @@ def what_to_put_on_bottom (hand, drawfirst, number_bottom, num_iterations):
 		hand_list += [card] * hand[card]
 	
 	#Iterate over all tuples of length number_bottom containing elements from hand_list 
-	#There may be duplicates right now, that's bad for runtime but shouldn't affect the maximum
-	for bottom in combinations(hand_list, number_bottom):
+	#Use set to avoid duplicates
+	for bottom in set(combinations(hand_list, number_bottom)):
 		#Transform back to dictionary for convenience
 		bottom_dict = {}
 		for card in decklist.keys():
@@ -163,28 +194,33 @@ def simulate_one_handsize(handsize, drawfirst):
 	Returns - the probability of achieving the goal with this opening hand size and play/draw setting
 	Note - for handsize > 1 the value of success_probability(handsize - 1) needs to be known!!!
 	"""
-	#The following numbers can be adjusted manually to increase/decrease total runtime
-	sample_size_for_num_hands = 4000 * handsize if handsize < 7 else 100000
-	sample_size_per_bottom = 4 * handsize
-	sample_size_per_hand_under_best_bottom = 8 * handsize
-	
+	#Initialize
 	count_probability = 0.0
 
-	#Construct library as a list
+	#The following number can be adjusted manually to increase/decrease total runtime
+	multiplier = 20000
+
+	#Construct library as a list, as well as the max-7 library from which to take possible opening hands
 	library = []
+	max_7_library = []
 	for card in decklist.keys():
 		library += [card] * decklist[card]
+		max_7_library += [card] * min(decklist[card], 7)
 	
-	for iterator in range(sample_size_for_num_hands):
+	#Iterate over all possible opening hands
+	for opening_hand_list in set(combinations(max_7_library, 7)):
 		
-		if( iterator > 100 and iterator % 1000 == 0):
-			print(f'We are now on hand number {iterator}. Current prob = {count_probability / iterator * 100 :.2f} %.')
+		#Feeding the list into "Counter" gives a dictionary with the number drawn for each cardtype
+		opening_hand = Counter(opening_hand_list)
+		probability = multivariate_hypgeom(decklist, Counter(opening_hand))
 		
-		#Construct a random opening hand
-		#Here, random.sample takes a random sample of 7 cards from library without replacement
-		#Feeding that sample into "Counter" gives a dictionary with the number drawn for each cardtype
-		opening_hand = Counter(random.sample(library, 7))
-	
+		#Determine sample sizes
+		sample_size_per_bottom = int(max(10, handsize * probability * multiplier))
+		sample_size_per_hand_under_best_bottom = int(max(20, handsize * probability * multiplier * 4))
+		if handsize == 7:
+			sample_size_per_bottom *= 5
+			sample_size_per_hand_under_best_bottom *= 5
+		
 		#Determine the set of cards that are best to put on the bottom
 		if (handsize < 7):
 			best_bottom = what_to_put_on_bottom(opening_hand, drawfirst, 7 - handsize, sample_size_per_bottom)
@@ -206,19 +242,59 @@ def simulate_one_handsize(handsize, drawfirst):
 			succes_prob_when_keep = simulate_one_specific_hand(opening_hand, best_bottom, drawfirst, sample_size_per_hand_under_best_bottom)
 			succes_prob_when_mull = success_probability[handsize - 1]
 			succes_prob = max(succes_prob_when_keep, succes_prob_when_mull)
-		count_probability += succes_prob
+		#print(f"Succes_prob {succes_prob} with hand: ", end='')
+		#print(opening_hand)
+		count_probability += succes_prob * probability
 		
-	return count_probability / sample_size_for_num_hands
+	return count_probability
 
-final_prob_for_7 = 0
-for drawfirst in [True, False]:
-	#Ugly initialization of the success_probability list
-	success_probability = [None] * 10
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 0, length = 100, fill = '█', printEnd = "\r", newline_on_complete=False):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete if specified
+    if iteration == total and newline_on_complete: 
+        print()
 
-	for handsize in range(1, 8):
-		print(f'We will now simulate handsize {handsize} when drawfirst is {drawfirst}.')
-		success_probability[handsize] = simulate_one_handsize(handsize,drawfirst)
-		print(f'The success probability for handsize {handsize} when drawfirst is {drawfirst}: { success_probability[handsize] * 100 :.2f} %.') 
-		if (handsize == 7):
-			final_prob_for_7 += success_probability[handsize]
-print(f'So when you sit down and have yet to roll the die, the number is {final_prob_for_7 * 50:.2f} %.')
+for Emrakul in [2, 3, 4, 5]:
+	decklist = {
+		'Trickery': 1,
+		'Outburst': 4,
+		'Emrakul': Emrakul,
+		'Land': 55 - Emrakul
+	}
+	print(decklist)
+	printProgressBar(0, 70, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	start_time = datetime.now()
+
+	final_prob_for_7 = 0
+	i = 0
+	for drawfirst in [True, False]:
+		#Ugly initialization of the success_probability list
+		success_probability = [None] * 10
+
+		for handsize in range(1, 8):
+			#print(f'We will now simulate handsize {handsize} when drawfirst is {drawfirst}.')
+			success_probability[handsize] = simulate_one_handsize(handsize,drawfirst)
+			#print(f'The success probability for handsize {handsize} when drawfirst is {drawfirst}: { success_probability[handsize] * 100 :.2f} %.') 
+			if (handsize == 7):
+				final_prob_for_7 += success_probability[handsize]
+				i += handsize
+			i += handsize
+			end_time = datetime.now()
+			printProgressBar(i, 70, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	print("Sims completed in", (end_time - start_time).seconds, end=' seconds')
+	print(f'\nSuccess probability (avg of play and draw): {final_prob_for_7 * 50:.3f} %.\n')
